30537410d243a7f63ce74e43a41994f9dfc2baee
Frederic Chien <fred_chien@yahoo.com.br>
2025-02-28 10:32:33 -0300

    feat: add repository to users

---
+++ src/use-cases/register.ts
@@ -0,0 +1,35 @@
+import { prisma } from "@/lib/prisma"
+import { hash } from "bcryptjs"
+import {PrismaUserRepository} from '@/repositories/prisma-users-repository'
+
+interface RegisterUseCaseRequest {
+    name: string
+    email: string
+    password: string
+}
+
+export async function registerUseCase({
+    name, 
+    email, 
+    password
+}: RegisterUseCaseRequest){
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    })
+
+    if(userWithSameEmail){
+        throw new Error('User with same email already exists')
+    }
+
+    const prismaUsersRepository = new PrismaUserRepository()
+
+    await prismaUsersRepository.create({
+        name,
+        email,
+        password_hash
+    })
+}
