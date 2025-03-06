ca2a8465132680e595b76653a3619aacf7f929fa
Frederic Chien <fred_chien@yahoo.com.br>
2025-03-06 10:29:37 -0300

    feat: add controller profile

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,29 @@
 import fastify from 'fastify'
+import fastifyJwt from '@fastify/jwt'
 import { appRoutes } from './http/routes'
 import { ZodError } from 'zod'
-import { env } from './env'
+import { env } from '@/env'
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
 
 app.register(appRoutes)
 
 app.setErrorHandler((error, _, reply) => {
-    if(error instanceof ZodError){
-        return reply
-        .status(400)
-        .send({message: 'Validation error.', issues: error.format()})
-    }
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
 
-    if(env.NODE_ENV !== 'production'){
-        console.error(error)
-    }else{
-        //TODO: Usar alguma ferramente externa para logar o erro DataDog/NewRelic/Sentry
-    }
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Usar alguma ferramente externa para logar o erro DataDog/NewRelic/Sentry
+  }
 
-    return reply.status(500).send({message: 'Internal server error'})
+  return reply.status(500).send({ message: 'Internal server error' })
 })
